049ee312d4725ea4bc8f050ee6ce0b85bbb2d9a4
QuanLe0702 <quanle0702@gmail.com>
2025-07-06 16:39:46 +0700

    update

--- server/server.js
+++ server/server.js
@@ -1,25 +1,25 @@
-const express = require("express")
-require("dotenv").config()
-const dbConnect = require("./config/dbconnect")
-const initRoutes = require("./routes")
-const cookieParser = require("cookie-parser")
-const cors = require("cors")
+const express = require("express");
+require("dotenv").config();
+const dbConnect = require("./config/dbconnect");
+const initRoutes = require("./routes");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
-const app = express()
+const app = express();
 app.use(
   cors({
     origin: process.env.CLIENT_URL,
     methods: ["POST", "PUT", "GET", "DELETE"],
     credentials: true,
   })
-)
-app.use(cookieParser())
-const port = process.env.PORT || 8888
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-dbConnect()
-initRoutes(app)
+);
+app.use(cookieParser());
+const port = process.env.PORT || 8888;
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+dbConnect();
+initRoutes(app);
 
-app.listen(port, () => {
-  console.log("Server running on the port: " + port)
-})
+app.listen(port, "0.0.0.0", () => {
+  console.log("Server running on the port: " + port);
+});
